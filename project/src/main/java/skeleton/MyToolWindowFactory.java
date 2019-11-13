76fb23c082c1d87f44eba9ad8a8097347e591c96
Taekang Eom <tkeom0114@postech.ac.kr>
2019-11-13 21:58:31 +0900

    build development environment

---
+++ project/src/main/java/skeleton/MyToolWindowFactory.java
@@ -0,0 +1,22 @@
+package skeleton;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBScrollBar;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A factory to create a Project Structure tool window.
+ */
+public class MyToolWindowFactory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(new JBScrollPane(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
---
+++ project/src/test/java/skeleton/ProjectStructureTest.java
@@ -0,0 +1,17 @@
+package skeleton;
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+
+
+
+public class ProjectStructureTest extends BasePlatformTestCase {
+
+    public void testProjectTreeModelRoot() {
+        assertEquals(0,0);
+    }
+
+    public void testProjectStructureTreeRoot() {
+        assertEquals(0,0);
+    }
+
+}
