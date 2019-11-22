ec0ae66758011f419528afa2c94cc901325f846f
Hyeon Jeon <jeonhyun97@gmail.com>
2019-11-23 01:04:45 +0900

    vim command / search popup implemented

--- project/src/main/java/project_Team7/MyTypedHandler.java
+++ project/src/main/java/project_Team7/MyTypedHandler.java
@@ -6,6 +6,8 @@ import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.ui.popup.JBPopupListener;
+import com.intellij.openapi.ui.popup.LightweightWindowEvent;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -21,45 +23,80 @@ import java.util.List;
 public class MyTypedHandler implements TypedActionHandler {
 
 
+
+    private String currentCommand = null;
+    private String currentSearchingString = null;
+
     private JPanel panel = null;
+
+    public String getCurrentCommand() {
+        return currentCommand;
+    }
 
     @Override
     public void execute(@NotNull Editor editor, char charTyped, @NotNull DataContext dataContext) {
-        if (charTyped == (':') && panel == null) {
-            panel = new JPanel(new BorderLayout());
-            JTextField textField = new JTextField(": ");
-            textField.setEditable(true);
-            textField.addKeyListener(new KeyListener() {
-                @Override
-                public void keyTyped(KeyEvent e) {
-                    System.out.println(textField.getText());
-                    System.out.println(e.getKeyChar());
-                }
-
-                @Override
-                public void keyPressed(KeyEvent e) {
-
-                }
-
-                @Override
-                public void keyReleased(KeyEvent e) {
-
-                }
-            });
-
-            panel.add(textField);
-
-
-
-            JBPopupFactory a = JBPopupFactory.getInstance();
-            JBPopup popup = a.createComponentPopupBuilder(panel, textField).createPopup();
-            popup.setRequestFocus(true);
-            popup.setSize(new Dimension(editor.getComponent().getWidth(), 10));
-            popup.showUnderneathOf(editor.getComponent());
-
-
-
+        if ((charTyped == ':' || charTyped == '/') && panel == null) {
+            keyStrokeCommandMode(charTyped + " ", editor);
         }
 
     }
+
+
+
+
+    private void keyStrokeCommandMode(String command, Editor editor) {
+
+        panel = new JPanel(new BorderLayout());
+        JTextField textField = new JTextField(command);
+
+        textField.setEditable(true);
+
+        textField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+                if(command.equals(": ")) {
+                    if(textField.getText().length() <= 2) {
+                        textField.setText(": ");
+                    }
+                    currentCommand = textField.getText().substring(1);
+                    System.out.println(currentCommand);
+                }
+                else {
+                    if(textField.getText().length() <= 2) {
+                        textField.setText("/ ");
+                    }
+                    currentSearchingString = textField.getText().substring(1);
+                    System.out.println(currentSearchingString);
+                }
+
+            }
+        });
+        panel.add(textField);
+
+
+        JBPopupFactory a = JBPopupFactory.getInstance();
+        JBPopup popup = a.createComponentPopupBuilder(panel, textField).createPopup();
+        popup.setRequestFocus(true);
+        popup.setSize(new Dimension(editor.getComponent().getWidth(), 10));
+        popup.showUnderneathOf(editor.getComponent());
+        popup.addListener(new JBPopupListener() {
+            @Override
+            public void onClosed(@NotNull LightweightWindowEvent event) {
+                panel = null;
+
+            }
+        });
+    }
+
 }
